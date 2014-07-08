6ed28dafe3ad5bde37dcd43b2939859288adf5da
Joao Stein <joaosteing@gmail.com>
2014-07-08 07:34:40 -0300

    start point

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Wordcup - Faça uma promessa que vai fazer a diferença e ajude o Brasil a ganhar muito mais que uma taça' });
 });
 
 module.exports = router;
